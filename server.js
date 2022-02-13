86f93c3d9272ee1cd6ceae5623f86e3f1f9816cb
panfilo macantan <olifnapnatnacam27.com>
2022-02-12 20:34:08 -0400

    added package cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors())
 const nameArray = [
     "Brixson Lajara",
     "Panfilo macantan",
