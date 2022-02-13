7e6ba05728cbd785401e822d86257e3b91f4f0ae
panfilo macantan <olifnapnatnacam27.com>
2022-02-12 20:39:50 -0400

    update cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 app.use(cors())
 const nameArray = [
     "Brixson Lajara",
