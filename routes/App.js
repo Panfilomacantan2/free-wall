1c68c93a66a28beab9cbe376ff31b4bf0d11a3d2
panfilo macantan <olifnapnatnacam27.com>
2022-03-14 08:38:16 -0300

    added database to mongodb

--- routes/App.js
+++ routes/App.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
+const mongoose = require("mongoose");
+const User = require("../model/User");
 
 router.get("/", (req, res) => {
-  const { id } = req.query;
-  res.send(`Hello! ${id}`);
+  User.find({}, (err, user) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(user);
+    }
+  });
 });
 
 router.post("/", (req, res) => {
-  res.send(`Hello ${req.query.id}`);
+  res.send(`Hello ${req.body.name}`);
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+  });
+
+  user
+    .save()
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.get("/:userId", (req, res) => {
--- server.js
+++ server.js
@@ -1,49 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const config = require("config");
+const dbConfig = config.get("Users.dbConfig.dbName");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-
-
-
-
-
-const mongoClient = require("mongodb").MongoClient;
-
-mongoClient.connect(
-  "mongodb+srv://programmix:<uaDInHdfItCAQrcE>@cluster0.3mo7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  (err, client) => {
-    if (err) {
-      console.log(err);
-    } else {  
-      console.log("Connected to MongoDB");
-    }
-    const db = client.db("test");
-    const collection = db.collection("users");
-    collection.insertOne({ name: "Brixson Lajara" });
-  }
-);
-
-
-app.use(cors());
-const nameArray = [
-  "Brixson Lajara",
-  "Panfilo macantan",
-  "Kevin Alcuetas",
-  "franz Arcebal Cayubit",
-  "This REST API is created by Panfilo macantan",
-  "This REST API is made of Nodejs, Express, and Mysql",
-];
-// Middleware
+const mongoose = require("mongoose");
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 //Import routes
 const AppRoutes = require("./routes/App");
-const createUserService = require('./Services/createUser');
+const createUserService = require("./Services/createUser");
 app.use("/post", AppRoutes);
 app.use("/", createUserService);
 
+app.use(cors());
+mongoose
+  .connect(dbConfig)
+  .then(() => { console.log("Connected to database!")})
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+// Middleware
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
