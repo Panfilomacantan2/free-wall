fea34abaaed9d9b72695d16179d711a30ac175c2
panfilo macantan <olifnapnatnacam27.com>
2022-08-13 12:29:11 -0300

    first commit

--- Services/createUser.js
+++ Services/createUser.js
@@ -1,16 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-const nameArray = [
-  "Brixson Lajara",
-  "Panfilo macantan",
-  "Kevin Alcuetas",
-  "franz Arcebal Cayubit",
-  "This REST API is created by Panfilo macantan",
-  "This REST API is made of Nodejs, Express, and Mysql",
-];
+
 router.get("/create", (req, res, next) => {
-  res.send(nameArray);
+  res.send("Hello World!");
 });
 
 export default router;
--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 
-import config from "config";
-const dbConfig = config.get("Users.dbConfig.dbName");
+import config from 'config';
+const dbConfig = config.get('Users.dbConfig.dbName');
 const port = process.env.PORT || 3000;
-import cors from "cors";
-import mongoose from "mongoose";
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 //Import routes
-import AppRoutes from "./routes/App.js";
-import createUserService from "./Services/createUser.js";
+import AppRoutes from './routes/App.js';
+import createUserService from './Services/createUser.js';
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/post", AppRoutes);
-app.use("/", createUserService);
+app.use('/post', AppRoutes);
+app.use('/', createUserService);
 
 //Connect to MongoDB
 const initializeDB = async () => {
-  try {
-    await mongoose.connect(dbConfig);
-    console.log("Connected to database!");
-  } catch (err) {
-    console.log("Connection failed error: " + err);
-  }
+	try {
+		await mongoose.connect(dbConfig);
+		console.log('Connected to database!');
+	} catch (err) {
+		console.log('Connection failed error: ' + err);
+	}
 };
 initializeDB();
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
